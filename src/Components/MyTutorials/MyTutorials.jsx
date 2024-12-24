1b69957b74b8ef11db9b7d35237640d784296c29
Biswa Mitra Barua <biswamitra960@gmail.com>
2024-12-25 01:16:01 +0600

    delete button functionalities imaplemented and showing confirmation and updated ui

--- src/Components/MyTutorials/MyTutorials.jsx
+++ src/Components/MyTutorials/MyTutorials.jsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { authContext } from '../AuthProvider/AuthProvider';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const MyTutorials = () => {
     const { user } = useContext(authContext);
-   
+
     const [tutorials, setTutorials] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -44,6 +45,39 @@ const MyTutorials = () => {
         return <div className="text-center text-red-500">{error.message}</div>;
     }
 
+    const handleDelete = (_id) => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/tutors/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your equipment has been deleted.",
+                                icon: "success"
+                            });
+                            const remaining = tutorials.filter(tut => tut._id !== _id)
+                            setTutorials(remaining)
+                        }
+                    })
+            }
+        });
+    }
+
 
     return (
         <div className='w-11/12 mx-auto'>
@@ -77,12 +111,12 @@ const MyTutorials = () => {
                                     <td>{tutorial.review}</td>
                                     <td>
                                         {/* <Link to={`/updateEquipment/${_id}`}> */}
-                                            <button className="btn btn-primary">Update</button>
+                                        <button className="btn btn-primary">Update</button>
                                         {/* </Link> */}
                                     </td>
                                     <td>
                                         <button
-                                            onClick={() => handleDelete(_id)}
+                                            onClick={() => handleDelete(tutorial._id)}
                                             className="btn btn-primary">Delete</button>
                                     </td>
                                 </tr>)
