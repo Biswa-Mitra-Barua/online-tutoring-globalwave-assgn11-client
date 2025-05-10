f5fb1e5ed07d725e44a48408e5f9316a73211db6
Biswa Mitra Barua <biswamitra960@gmail.com>
2025-05-10 12:56:42 +0600

    working on the cards section

--- src/Components/LanguageCategory/LanguageCategory.jsx
+++ src/Components/LanguageCategory/LanguageCategory.jsx
@@ -18,7 +18,7 @@ const LanguageCategory = () => {
     ];
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10 mb-10 px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-10 mb-10 px-4 sm:px-6 lg:px-8">
             {categories.map((category, index) => (
                 <div
                     key={index}
